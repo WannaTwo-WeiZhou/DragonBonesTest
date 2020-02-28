1fa997b3a1ac12e3e3cfe4c7dab8abb4067595a1
WannaTwo <WannaTwo-WeiZhou@users.noreply.github.com>
2020-02-28 16:01:24 +0800

    hide some ui and change slot change

--- out/MainScene.js
+++ out/MainScene.js
@@ -19,8 +19,6 @@ var MainScene = /** @class */ (function (_super) {
         _this.factory = null;
         _this.arms = [];
         _this.armNum = 0;
-        _this.armNumOfAni = 0;
-        _this.armNumOfSlot = 0;
         _this.slotNumOneChange = 0;
         _this.slotChangeIdx = 0;
         return _this;
@@ -41,21 +39,22 @@ var MainScene = /** @class */ (function (_super) {
         var textStyle = { fontSize: 18, color: "#FFFFFF", align: "center" };
         // fps
         this.fpsText = this.add.text(10, 0, "FPS: --", textStyle);
+        this.fpsText.depth = 999;
         // armature num
         this.armNumText = this.add.text(40, 40, "ArmNum: 0", textStyle);
         var addArmNumBtn = this.add.image(160, 40, "sprites", "increase.png");
         var minArmNumBtn = this.add.image(15, 40, "sprites", "reduce.png");
         // armature num of animation ctrl
-        this.armNumOfAniText = this.add.text(40, 80, "ArmNumOfAni: 0", textStyle);
-        var addArmNumOfAniBtn = this.add.image(160, 80, "sprites", "increase.png");
-        var minArmNumOfAniBtn = this.add.image(15, 80, "sprites", "reduce.png");
+        // this.armNumOfAniText = this.add.text(40, 80, "ArmNumOfAni: 0", textStyle);
+        // let addArmNumOfAniBtn = this.add.image(160, 80, "sprites", "increase.png");
+        // let minArmNumOfAniBtn = this.add.image(15, 80, "sprites", "reduce.png");
         // animation ctrl
         var aniCtrlText = this.add.text(15, 120, "AniCtrl", textStyle);
         var aniCtrlBtn = this.add.image(120, 120, "sprites", "increase.png");
         // armature num of slot ctrl
-        this.armNumOfSlotText = this.add.text(40, 160, "ArmNumOfSlot: 0", textStyle);
-        var addArmNumOfSlotBtn = this.add.image(160, 160, "sprites", "increase.png");
-        var minArmNumOfSlotBtn = this.add.image(15, 160, "sprites", "reduce.png");
+        // this.armNumOfSlotText = this.add.text(40, 160, "ArmNumOfSlot: 0", textStyle);
+        // let addArmNumOfSlotBtn = this.add.image(160, 160, "sprites", "increase.png");
+        // let minArmNumOfSlotBtn = this.add.image(15, 160, "sprites", "reduce.png");
         // slot ctrl num
         this.slotNumOneChangeText = this.add.text(40, 200, "SlotNumOneChange: 0", textStyle);
         var addSlotOneChangeBtn = this.add.image(160, 200, "sprites", "increase.png");
@@ -68,16 +67,16 @@ var MainScene = /** @class */ (function (_super) {
         addArmNumBtn.on("pointerdown", this.addArmNum, this);
         minArmNumBtn.setInteractive();
         minArmNumBtn.on("pointerdown", this.minArmNum, this);
-        addArmNumOfAniBtn.setInteractive();
-        addArmNumOfAniBtn.on("pointerdown", this.addArmNumOfAni, this);
-        minArmNumOfAniBtn.setInteractive();
-        minArmNumOfAniBtn.on("pointerdown", this.minArmNumOfAni, this);
+        // addArmNumOfAniBtn.setInteractive();
+        // addArmNumOfAniBtn.on("pointerdown", this.addArmNumOfAni, this);
+        // minArmNumOfAniBtn.setInteractive();
+        // minArmNumOfAniBtn.on("pointerdown", this.minArmNumOfAni, this);
         aniCtrlBtn.setInteractive();
         aniCtrlBtn.on("pointerdown", this.ctrlAni, this);
-        addArmNumOfSlotBtn.setInteractive();
-        addArmNumOfSlotBtn.on("pointerdown", this.addArmNumOfSlot, this);
-        minArmNumOfSlotBtn.setInteractive();
-        minArmNumOfSlotBtn.on("pointerdown", this.minArmNumOfSlot, this);
+        // addArmNumOfSlotBtn.setInteractive();
+        // addArmNumOfSlotBtn.on("pointerdown", this.addArmNumOfSlot, this);
+        // minArmNumOfSlotBtn.setInteractive();
+        // minArmNumOfSlotBtn.on("pointerdown", this.minArmNumOfSlot, this);
         addSlotOneChangeBtn.setInteractive();
         addSlotOneChangeBtn.on("pointerdown", this.addSlotOneChange, this);
         minSlotOneChangeBtn.setInteractive();
@@ -91,11 +90,11 @@ var MainScene = /** @class */ (function (_super) {
     };
     // change armature number
     MainScene.prototype.addArmNum = function () {
-        this.updateArmNum(1);
+        this.updateArmNum(10);
         this.debugOperation("add armature num");
     };
     MainScene.prototype.minArmNum = function () {
-        this.updateArmNum(-1);
+        this.updateArmNum(-10);
         this.debugOperation("minus armature num");
     };
     MainScene.prototype.updateArmNum = function (val) {
@@ -107,64 +106,64 @@ var MainScene = /** @class */ (function (_super) {
         // add
         while (this.armNum > this.arms.length) {
             var arm = this.add.armature("mecha_1004d", "mecha_1004d");
-            arm.x = 200 + Math.random() * (maxX - 200);
-            arm.y = Math.random() * maxY;
+            arm.x = 300 + Math.random() * (maxX - 300);
+            arm.y = 100 + Math.random() * (maxY - 100);
             arm.scale = 0.25;
             this.arms.push(arm);
         }
         // minus
         while (this.armNum < this.arms.length) {
             var arm = this.arms.pop;
-            arm.destroy();
+            arm.dispose();
         }
     };
     // change armature num of animation ctrl
-    MainScene.prototype.addArmNumOfAni = function () {
-        this.updateArmNumOfAni(1);
-        this.debugOperation("add armature num of animation ctrl");
-    };
-    MainScene.prototype.minArmNumOfAni = function () {
-        this.updateArmNumOfAni(-1);
-        this.debugOperation("minus armature num of animation ctrl");
-    };
-    MainScene.prototype.updateArmNumOfAni = function (val) {
-        this.armNumOfAni += val;
-        this.armNumOfAni = this.armNumOfAni > this.armNum ? this.armNum : this.armNumOfAni;
-        this.armNumOfAni = this.armNumOfAni < 0 ? 0 : this.armNumOfAni;
-        this.armNumOfAniText.text = "ArmNumOfAni: " + this.armNumOfAni;
-        // stop all animation
-        for (var i = 0; i < this.arms.length; i++) {
-            var element = this.arms[i];
-            if (element.animation.isPlaying)
-                element.animation.stop();
-        }
-    };
+    // private addArmNumOfAni() {
+    //     this.updateArmNumOfAni(1);
+    //     this.debugOperation("add armature num of animation ctrl");
+    // }
+    // private minArmNumOfAni() {
+    //     this.updateArmNumOfAni(-1);
+    //     this.debugOperation("minus armature num of animation ctrl");
+    // }
+    // private updateArmNumOfAni(val: number) {
+    //     this.armNumOfAni += val;
+    //     this.armNumOfAni = this.armNumOfAni > this.armNum ? this.armNum : this.armNumOfAni;
+    //     this.armNumOfAni = this.armNumOfAni < 0 ? 0 : this.armNumOfAni;
+    //     this.armNumOfAniText.text = "ArmNumOfAni: " + this.armNumOfAni;
+    //     // stop all animation
+    //     for (let i = 0; i < this.arms.length; i++) {
+    //         const element = this.arms[i];
+    //         if (element.animation.isPlaying) element.animation.stop();
+    //     }
+    // }
     // animation ctrl
     MainScene.prototype.ctrlAni = function () {
         var isPlaying = this.arms.length > 0 && this.arms[0].animation.isPlaying;
-        for (var i = 0; i < this.armNumOfAni; i++) {
+        for (var i = 0; i < this.arms.length; i++) {
             var element = this.arms[i];
             if (isPlaying)
                 element.animation.stop();
             else
                 element.animation.play();
         }
+        this.debugOperation("change animation playing");
     };
     // change armature num of slot ctrl
-    MainScene.prototype.addArmNumOfSlot = function () {
-        this.updateArmNumOfSlot(1);
-        this.debugOperation("add armature num of slot ctrl");
-    };
-    MainScene.prototype.minArmNumOfSlot = function () {
-        this.updateArmNumOfSlot(-1);
-        this.debugOperation("minus armature num of slot ctrl");
-    };
-    MainScene.prototype.updateArmNumOfSlot = function (val) {
-        this.armNumOfSlot += val;
-        this.armNumOfSlot = this.armNumOfSlot > this.armNum ? this.armNum : this.armNumOfSlot;
-        this.armNumOfSlot = this.armNumOfSlot < 0 ? 0 : this.armNumOfSlot;
-        this.armNumOfSlotText.text = "ArmNumOfSlot: " + this.armNumOfSlot;
-    };
+    // private addArmNumOfSlot() {
+    //     this.updateArmNumOfSlot(1);
+    //     this.debugOperation("add armature num of slot ctrl");
+    // }
+    // private minArmNumOfSlot() {
+    //     this.updateArmNumOfSlot(-1);
+    //     this.debugOperation("minus armature num of slot ctrl");
+    // }
+    // private updateArmNumOfSlot(val: number) {
+    //     this.armNumOfSlot += val;
+    //     this.armNumOfSlot = this.armNumOfSlot > this.armNum ? this.armNum : this.armNumOfSlot;
+    //     this.armNumOfSlot = this.armNumOfSlot < 0 ? 0 : this.armNumOfSlot;
+    //     this.armNumOfSlotText.text = "ArmNumOfSlot: " + this.armNumOfSlot;
+    // }
     // change slot ctrl num
     MainScene.prototype.addSlotOneChange = function () {
         this.updateSlotOneChange(1);
@@ -184,7 +183,7 @@ var MainScene = /** @class */ (function (_super) {
     MainScene.prototype.ctrlSlot = function () {
         this.slotChangeIdx++;
         this.slotChangeIdx %= MainScene.WEAPONLIST.length;
-        for (var i = 0; i < this.armNumOfSlot; i++) {
+        for (var i = 0; i < this.arms.length; i++) {
             var element = this.arms[i];
             if (this.slotNumOneChange > 0) {
                 // 右手武器素材在weapon_1004中
@@ -192,10 +191,12 @@ var MainScene = /** @class */ (function (_super) {
                 this.factory.replaceSlotDisplay("weapon_1004", "weapon", "weapon_r", displayName, element.armature.getSlot("weapon_hand_r"));
                 if (this.slotNumOneChange > 1) {
                     // 左手武器素材在mecha_1004d中
-                    element.armature.getSlot("weapon_hand_l").displayIndex = this.slotChangeIdx;
+                    // element.armature.getSlot("weapon_hand_l").displayIndex = this.slotChangeIdx;
+                    this.factory.replaceSlotDisplay("weapon_1004", "weapon", "weapon_r", displayName, element.armature.getSlot("weapon_hand_l"));
                 }
             }
         }
+        this.debugOperation("change slot");
     };
     MainScene.BACKGROUND_URL = "resource/background.png";
     MainScene.MAXSLOTNUMONECHANGE = 2;
--- src/MainScene.ts
+++ src/MainScene.ts
@@ -10,10 +10,10 @@ class MainScene extends Phaser.Scene {
     private arms: dragonBones.phaser.display.ArmatureDisplay[] = [];
     private armNumText: Phaser.GameObjects.Text;
     private armNum: number = 0;
-    private armNumOfAniText: Phaser.GameObjects.Text;
-    private armNumOfAni: number = 0;
-    private armNumOfSlotText: Phaser.GameObjects.Text;
-    private armNumOfSlot: number = 0;
+    // private armNumOfAniText: Phaser.GameObjects.Text;
+    // private armNumOfAni: number = 0;
+    // private armNumOfSlotText: Phaser.GameObjects.Text;
+    // private armNumOfSlot: number = 0;
     private slotNumOneChangeText: Phaser.GameObjects.Text;
     private slotNumOneChange: number = 0;
     private slotChangeIdx: number = 0;
@@ -54,21 +54,22 @@ class MainScene extends Phaser.Scene {
         const textStyle = { fontSize: 18, color: "#FFFFFF", align: "center" };
         // fps
         this.fpsText = this.add.text(10, 0, "FPS: --", textStyle);
+        this.fpsText.depth = 999;
         // armature num
         this.armNumText = this.add.text(40, 40, "ArmNum: 0", textStyle);
-        let addArmNumBtn = this.add.image(160, 40, "sprites", "increase.png");
+        let addArmNumBtn = this.add.image(200, 40, "sprites", "increase.png");
         let minArmNumBtn = this.add.image(15, 40, "sprites", "reduce.png");
         // armature num of animation ctrl
-        this.armNumOfAniText = this.add.text(40, 80, "ArmNumOfAni: 0", textStyle);
-        let addArmNumOfAniBtn = this.add.image(160, 80, "sprites", "increase.png");
-        let minArmNumOfAniBtn = this.add.image(15, 80, "sprites", "reduce.png");
+        // this.armNumOfAniText = this.add.text(40, 80, "ArmNumOfAni: 0", textStyle);
+        // let addArmNumOfAniBtn = this.add.image(160, 80, "sprites", "increase.png");
+        // let minArmNumOfAniBtn = this.add.image(15, 80, "sprites", "reduce.png");
         // animation ctrl
         let aniCtrlText = this.add.text(15, 120, "AniCtrl", textStyle);
         let aniCtrlBtn = this.add.image(120, 120, "sprites", "increase.png");
         // armature num of slot ctrl
-        this.armNumOfSlotText = this.add.text(40, 160, "ArmNumOfSlot: 0", textStyle);
-        let addArmNumOfSlotBtn = this.add.image(160, 160, "sprites", "increase.png");
-        let minArmNumOfSlotBtn = this.add.image(15, 160, "sprites", "reduce.png");
+        // this.armNumOfSlotText = this.add.text(40, 160, "ArmNumOfSlot: 0", textStyle);
+        // let addArmNumOfSlotBtn = this.add.image(160, 160, "sprites", "increase.png");
+        // let minArmNumOfSlotBtn = this.add.image(15, 160, "sprites", "reduce.png");
         // slot ctrl num
         this.slotNumOneChangeText = this.add.text(40, 200, "SlotNumOneChange: 0", textStyle);
         let addSlotOneChangeBtn = this.add.image(160, 200, "sprites", "increase.png");
@@ -82,16 +83,16 @@ class MainScene extends Phaser.Scene {
         addArmNumBtn.on("pointerdown", this.addArmNum, this);
         minArmNumBtn.setInteractive();
         minArmNumBtn.on("pointerdown", this.minArmNum, this);
-        addArmNumOfAniBtn.setInteractive();
-        addArmNumOfAniBtn.on("pointerdown", this.addArmNumOfAni, this);
-        minArmNumOfAniBtn.setInteractive();
-        minArmNumOfAniBtn.on("pointerdown", this.minArmNumOfAni, this);
+        // addArmNumOfAniBtn.setInteractive();
+        // addArmNumOfAniBtn.on("pointerdown", this.addArmNumOfAni, this);
+        // minArmNumOfAniBtn.setInteractive();
+        // minArmNumOfAniBtn.on("pointerdown", this.minArmNumOfAni, this);
         aniCtrlBtn.setInteractive();
         aniCtrlBtn.on("pointerdown", this.ctrlAni, this);
-        addArmNumOfSlotBtn.setInteractive();
-        addArmNumOfSlotBtn.on("pointerdown", this.addArmNumOfSlot, this);
-        minArmNumOfSlotBtn.setInteractive();
-        minArmNumOfSlotBtn.on("pointerdown", this.minArmNumOfSlot, this);
+        // addArmNumOfSlotBtn.setInteractive();
+        // addArmNumOfSlotBtn.on("pointerdown", this.addArmNumOfSlot, this);
+        // minArmNumOfSlotBtn.setInteractive();
+        // minArmNumOfSlotBtn.on("pointerdown", this.minArmNumOfSlot, this);
         addSlotOneChangeBtn.setInteractive();
         addSlotOneChangeBtn.on("pointerdown", this.addSlotOneChange, this);
         minSlotOneChangeBtn.setInteractive();
@@ -107,11 +108,11 @@ class MainScene extends Phaser.Scene {
 
     // change armature number
     private addArmNum() {
-        this.updateArmNum(1);
+        this.updateArmNum(10);
         this.debugOperation("add armature num");
     }
     private minArmNum() {
-        this.updateArmNum(-1);
+        this.updateArmNum(-10);
         this.debugOperation("minus armature num");
     }
     private updateArmNum(val: number) {
@@ -124,67 +125,68 @@ class MainScene extends Phaser.Scene {
         // add
         while (this.armNum > this.arms.length) {
             let arm = this.add.armature("mecha_1004d", "mecha_1004d");
-            arm.x = 200 + Math.random() * (maxX - 200);
-            arm.y = Math.random() * maxY;
+            arm.x = 300 + Math.random() * (maxX - 300);
+            arm.y = 100 + Math.random() * (maxY - 100);
             arm.scale = 0.25;
             this.arms.push(arm);
         }
         // minus
         while (this.armNum < this.arms.length) {
             let arm = this.arms.pop;
-            arm.destroy();
+            arm.dispose();
         }
 
     }
 
     // change armature num of animation ctrl
-    private addArmNumOfAni() {
-        this.updateArmNumOfAni(1);
-        this.debugOperation("add armature num of animation ctrl");
-    }
-    private minArmNumOfAni() {
-        this.updateArmNumOfAni(-1);
-        this.debugOperation("minus armature num of animation ctrl");
-    }
-    private updateArmNumOfAni(val: number) {
-        this.armNumOfAni += val;
-        this.armNumOfAni = this.armNumOfAni > this.armNum ? this.armNum : this.armNumOfAni;
-        this.armNumOfAni = this.armNumOfAni < 0 ? 0 : this.armNumOfAni;
-        this.armNumOfAniText.text = "ArmNumOfAni: " + this.armNumOfAni;
+    // private addArmNumOfAni() {
+    //     this.updateArmNumOfAni(1);
+    //     this.debugOperation("add armature num of animation ctrl");
+    // }
+    // private minArmNumOfAni() {
+    //     this.updateArmNumOfAni(-1);
+    //     this.debugOperation("minus armature num of animation ctrl");
+    // }
+    // private updateArmNumOfAni(val: number) {
+    //     this.armNumOfAni += val;
+    //     this.armNumOfAni = this.armNumOfAni > this.armNum ? this.armNum : this.armNumOfAni;
+    //     this.armNumOfAni = this.armNumOfAni < 0 ? 0 : this.armNumOfAni;
+    //     this.armNumOfAniText.text = "ArmNumOfAni: " + this.armNumOfAni;
 
-        // stop all animation
-        for (let i = 0; i < this.arms.length; i++) {
-            const element = this.arms[i];
-            if (element.animation.isPlaying) element.animation.stop();
-        }
-    }
+    //     // stop all animation
+    //     for (let i = 0; i < this.arms.length; i++) {
+    //         const element = this.arms[i];
+    //         if (element.animation.isPlaying) element.animation.stop();
+    //     }
+    // }
     // animation ctrl
     private ctrlAni() {
         let isPlaying: boolean = this.arms.length > 0 && this.arms[0].animation.isPlaying;
 
-        for (let i = 0; i < this.armNumOfAni; i++) {
+        for (let i = 0; i < this.arms.length; i++) {
             const element = this.arms[i];
             if (isPlaying)
                 element.animation.stop();
             else
                 element.animation.play();
         }
+        this.debugOperation("change animation playing");
     }
     // change armature num of slot ctrl
-    private addArmNumOfSlot() {
-        this.updateArmNumOfSlot(1);
-        this.debugOperation("add armature num of slot ctrl");
-    }
-    private minArmNumOfSlot() {
-        this.updateArmNumOfSlot(-1);
-        this.debugOperation("minus armature num of slot ctrl");
-    }
-    private updateArmNumOfSlot(val: number) {
-        this.armNumOfSlot += val;
-        this.armNumOfSlot = this.armNumOfSlot > this.armNum ? this.armNum : this.armNumOfSlot;
-        this.armNumOfSlot = this.armNumOfSlot < 0 ? 0 : this.armNumOfSlot;
-        this.armNumOfSlotText.text = "ArmNumOfSlot: " + this.armNumOfSlot;
-    }
+    // private addArmNumOfSlot() {
+    //     this.updateArmNumOfSlot(1);
+    //     this.debugOperation("add armature num of slot ctrl");
+    // }
+    // private minArmNumOfSlot() {
+    //     this.updateArmNumOfSlot(-1);
+    //     this.debugOperation("minus armature num of slot ctrl");
+    // }
+    // private updateArmNumOfSlot(val: number) {
+    //     this.armNumOfSlot += val;
+    //     this.armNumOfSlot = this.armNumOfSlot > this.armNum ? this.armNum : this.armNumOfSlot;
+    //     this.armNumOfSlot = this.armNumOfSlot < 0 ? 0 : this.armNumOfSlot;
+    //     this.armNumOfSlotText.text = "ArmNumOfSlot: " + this.armNumOfSlot;
+    // }
     // change slot ctrl num
     private addSlotOneChange() {
         this.updateSlotOneChange(1);
@@ -204,7 +206,7 @@ class MainScene extends Phaser.Scene {
     private ctrlSlot() {
         this.slotChangeIdx++;
         this.slotChangeIdx %= MainScene.WEAPONLIST.length;
-        for (let i = 0; i < this.armNumOfSlot; i++) {
+        for (let i = 0; i < this.arms.length; i++) {
             const element = this.arms[i];
             if (this.slotNumOneChange > 0) {
                 // 右手武器素材在weapon_1004中
@@ -212,9 +214,11 @@ class MainScene extends Phaser.Scene {
                 this.factory.replaceSlotDisplay("weapon_1004", "weapon", "weapon_r", displayName, element.armature.getSlot("weapon_hand_r"));
                 if (this.slotNumOneChange > 1) {
                     // 左手武器素材在mecha_1004d中
-                    element.armature.getSlot("weapon_hand_l").displayIndex = this.slotChangeIdx;
+                    // element.armature.getSlot("weapon_hand_l").displayIndex = this.slotChangeIdx;
+                    this.factory.replaceSlotDisplay("weapon_1004", "weapon", "weapon_r", displayName, element.armature.getSlot("weapon_hand_l"));
                 }
             }
         }
+        this.debugOperation("change slot");
     }
 }
